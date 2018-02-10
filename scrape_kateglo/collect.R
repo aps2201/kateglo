595e6ed4b45db56847f6751b32a7b8a787868582
aps2201 <halo@aps.web.id>
2018-02-10 09:32:38 +0700

    simplify list lema

--- scrape_kateglo/collect.R
+++ scrape_kateglo/collect.R
@@ -8,7 +8,8 @@ kategloAPI = function(phrase){
   build_dataframe = data_frame(phrase=get_list$phrase,phrase_type=get_list$phrase_type,lex_class=get_list$lex_class,lex_class_name=get_list$lex_class_name,ref_source=get_list$ref_source,ref_source_name=get_list$ref_source_name)
   build_dataframe
 }
-y = c[!is.na(c$type),]
-keterangan_kata = lapply(y$lema,kategloAPI)
+y = c$lema[!is.na(c$type)]
+y = gsub(" ","%20",y)
+keterangan_kata = lapply(y,kategloAPI)
 keterangan_kata = do.call(rbind,keterangan_kata)
 write_csv(keterangan_kata,"keterangan_kata.csv")
